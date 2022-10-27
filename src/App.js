8ced262656fdcd4e0ddf50ce08904bc31fc43170
dread557 <dadaolawunmi09@gmail.com>
2022-10-27 12:58:23 +0100

    Install needed dependencies and added images

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import './App.scss'
+import { Link } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
+      <header>
+
+      </header>
+      <main className='links'>
+
+      </main>
+      <footer>
+
+      </footer>
     </div>
   );
 }
